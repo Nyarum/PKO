66f786328d204edb34ba8ccd7a0371b0012c0bd2
Grigorii Eleskin <nyarumilian@gmail.com>
2024-11-22 19:17:43 +0400

    feat: print only last error

--- Run.jl
+++ Run.jl
@@ -1,19 +1,12 @@
 using Printf
 
-function print_all_errors_for_vscode(e, parent_file="", parent_line=0)
+function print_last_error_for_vscode(e, parent_file="", parent_line=0)
     # Check if the error is a LoadError
     if e isa LoadError
-        type_error = typeof(e.error)
-
-        # Print the current LoadError
-        println("$(e.file):$(e.line) - $(type_error)")
-        # Recursively process the nested error
-        print_all_errors_for_vscode(e.error, e.file, e.line)
-    elseif e isa UndefVarError
-        # Print UndefVarError
-        println("$parent_file:$parent_line - $(e.var)")
+        # Process the nested error
+        return print_last_error_for_vscode(e.error, e.file, e.line)
     else
-        # Handle generic errors
+        # Handle the final error (deepest level)
         println("$parent_file:$parent_line - $(e)")
     end
 end
@@ -22,5 +15,5 @@ try
     # Run your script or code
     include("src/PKO.jl")  # Replace this with your actual script or function
 catch e
-    print_all_errors_for_vscode(e)
+    print_last_error_for_vscode(e)
 end
